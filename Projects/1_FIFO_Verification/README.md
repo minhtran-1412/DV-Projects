08f6328cb38083ed0d69b97c5fd39c9ce11e38b4
Minh Tran <minhtranvuanh@gmail.com>
2026-07-30 16:40:37 +0700

    Implement basic synchronous FIFO RTL and testbench

---
+++ Projects/1_FIFO_Verification/README.md
@@ -0,0 +1,29 @@
+# FIFO Verification
+
+## Description
+
+Implemented a parameterized synchronous FIFO in SystemVerilog.
+
+## Features
+
+- Parameterized DATA_WIDTH and DEPTH
+- Write operation
+- Read operation
+- Full flag
+- Empty flag
+- Simple SystemVerilog testbench
+
+## Status
+
+🚧 In Progress
+
+## Waveform
+
+![Waveform](C:\Users\Meo Meo\OneDrive\Máy tính\,,,\Projects DV\Projects\Images\Result Project 1.png)
+
+Next:
+
+- Random stimulus
+- Scoreboard
+- Functional coverage
+- Assertions
--- Projects/1_FIFO_Verification/RTL/FIFO.sv
+++ Projects/1_FIFO_Verification/RTL/FIFO.sv
@@ -1,17 +1,64 @@
-module FIFO #(
-    parameter DEPTH = 8,
-    parameter DATA_WIDTH = 8
-) (
-    input  logic         clk,
-    input  logic         rst_n,
+`timescale 1ns/1ps
 
-    input  logic         wr_en,
-    input  logic         rd_en,
-
+module fifo #(
+    parameter DATA_WIDTH = 8,
+    parameter DEPTH      = 8
+)(
+    input  logic                  clk,
+    input  logic                  rst_n,
+    input  logic                  wr_en,
+    input  logic                  rd_en,
     input  logic [DATA_WIDTH-1:0] din,
-    output logic [DATA_WIDTH-1:0] dout,
 
-    output logic         full,
-    output logic         empty
+    output logic [DATA_WIDTH-1:0] dout,
+    output logic                  full,
+    output logic                  empty
 );
-endmodule 
+
+    localparam ADDR_WIDTH = $clog2(DEPTH);
+
+    logic [DATA_WIDTH-1:0] mem [0:DEPTH-1];
+
+    logic [ADDR_WIDTH-1:0] wr_ptr;
+    logic [ADDR_WIDTH-1:0] rd_ptr;
+
+    logic [ADDR_WIDTH:0] count;
+
+    assign full  = (count == DEPTH);
+    assign empty = (count == 0);
+
+    always_ff @(posedge clk or negedge rst_n) begin
+        if(!rst_n) begin
+            wr_ptr <= 0;
+            rd_ptr <= 0;
+            count  <= 0;
+            dout   <= 0;
+        end
+        else begin
+
+            // Write only
+            if(wr_en && !full && !(rd_en && !empty)) begin
+                mem[wr_ptr] <= din;
+                wr_ptr <= wr_ptr + 1;
+                count <= count + 1;
+            end
+
+            // Read only
+            else if(rd_en && !empty && !(wr_en && !full)) begin
+                dout <= mem[rd_ptr];
+                rd_ptr <= rd_ptr + 1;
+                count <= count - 1;
+            end
+
+            // Read + Write simultaneously
+            else if(wr_en && rd_en && !full && !empty) begin
+                mem[wr_ptr] <= din;
+                dout <= mem[rd_ptr];
+                wr_ptr <= wr_ptr + 1;
+                rd_ptr <= rd_ptr + 1;
+            end
+
+        end
+    end
+
+endmodule
